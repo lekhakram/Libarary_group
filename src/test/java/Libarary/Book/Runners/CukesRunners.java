34852bbf2c51737d0be523af852658b74d796310
lekhakram <98857287+lekhakram@users.noreply.github.com>
2022-08-08 20:32:00 -0400

    Update CukesRunners.java

--- src/test/java/Libarary/Book/Runners/CukesRunners.java
+++ src/test/java/Libarary/Book/Runners/CukesRunners.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "json:target/cucumber-report.json"
+                "json:target/cucumber.json"
         },
         features = "src/test/resources",
         glue="Libarary/Book/StepDefinations",
